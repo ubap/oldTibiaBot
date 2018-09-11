c1e5b263e2cfbd0888ab135e00470104cb519553
Jakub Trzebiatowski <jaktrzeb@gmail.com>
2018-09-11 23:56:14 +0200

    add example test

---
+++ controller/src/test/java/controller/ExampleTest.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        // assert statements
+        assertEquals(0, 1);
+    }
+}
